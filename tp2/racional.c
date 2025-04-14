bf6d98eadb534345e5b1d41524991e60185a43c1
Leonardo <leonardomartinslopes2@gmail.com>
2025-04-14 15:19:21 -0300

    [add] Adaptacao das funcoes

--- tp2/racional.c
+++ tp2/racional.c
@@ -10,6 +10,8 @@
 
 /* coloque aqui seus includes (primeiro os <...>, depois os "...") */
 #include <stdio.h>
+#include <stdlib.h>
+#include "racional.h"
 
 /*
  * Implemente aqui as funcoes definidas no racionais.h; caso precise,
@@ -20,21 +22,37 @@
 /* retorna um número aleatório entre min e max, inclusive. */
 long aleat (long min, long max)
 {
-  /* implemente aqui */
+    return rand() % (max - min + 1) + min;
 }
 
 /* Máximo Divisor Comum entre a e b      */
 /* calcula o MDC pelo método de Euclides */
 long mdc (long a, long b)
 {
-  /* implemente aqui */
+    long tmp_a,tmp_b;
+	tmp_a = labs(a);
+	tmp_b = labs(b);
+	while((tmp_a != 0) && (tmp_b != 0)){
+		if(tmp_a > tmp_b)
+			tmp_a = tmp_a % tmp_b;
+		else tmp_b = tmp_b % tmp_a;
+		}
+		if (tmp_a == 0)
+			return tmp_b;
+		else return tmp_a;
 }
 
 /* Mínimo Múltiplo Comum entre a e b */
 /* mmc = (a * b) / mdc (a, b)        */
 long mmc (long a, long b)
 {
-  /* implemente aqui */
+    long resultado;
+
+    resultado = mdc(a, b);
+	if (resultado < 0)
+		return -1;
+
+	return ((a * b) / resultado);
 }
 
 /* Recebe um número racional e o simplifica.
@@ -44,7 +62,129 @@ long mmc (long a, long b)
  * Se r for inválido, devolve-o sem simplificar. */
 struct racional simplifica_r (struct racional r)
 {
-  /* implemente aqui */
+    if (r.den == 0)
+        return r;
+
+    long maior_mult = mdc(r.num, r.den);
+    r.num = r.num / maior_mult;
+    r.den = r.den / maior_mult;
+    return r;
 }
 
-/* implemente as demais funções de racional.h aqui */
+int valido_r (struct racional r)
+{
+	//retorna 1 se for valido
+	if (r.den != 0)
+		return 1;
+
+	return 0;
+}
+
+struct racional cria_r (long numerador, long denominador)
+{
+	struct racional r;
+	r.num = numerador;
+	r.den = denominador;
+	
+	return r;
+}
+
+struct racional sorteia_r (long min, long max)
+{
+	struct racional r;
+	long num = aleat(min, max);
+	long den = aleat(min, max);
+
+	r = cria_r(num, den);
+	simplifica_r(r);
+
+	return r;
+}
+
+void imprime_r (struct racional r)
+{
+	r = simplifica_r(r);
+
+	if (r.den == 0){
+		printf (" INVALIDO");
+		return;
+	}
+	if (r.num == 0){
+		printf (" 0");
+		return;
+	}
+	if (r.num == r.den){
+		printf (" 1");
+		return;
+	}
+	//inverte se o den for negativo
+	if (r.den < 0){
+		r.num = r.num * -1;
+		r.den = r.den * -1;
+	}
+	if (r.den == 1){
+		printf (" %ld", r.num);
+		return;
+	}
+	printf (" %ld/%ld", r.num, r.den);
+	return;
+}
+
+int soma_r (struct racional r1, struct racional r2, struct racional *r3)
+{
+    r3 -> num = r1.num*r2.den + r2.num*r1.den;
+    r3 -> den = r1.den*r2.den;
+    *r3 = simplifica_r(*r3);
+    if (valido_r(r1)==0 || (valido_r(r2)==0) || r3 == NULL)
+        return 0;
+    else return 1;
+}
+
+int subtrai_r (struct racional r1, struct racional r2, struct racional *r3)
+{
+    r3 -> num = r1.num*r2.den - r2.num*r1.den;
+    r3 -> den = r1.den*r2.den;
+    if (valido_r(r1)==0 || (valido_r(r2)==0) || r3 == NULL)
+        return 0;
+    else return 1;
+}
+
+int multiplica_r (struct racional r1, struct racional r2, struct racional *r3)
+{
+    r3 -> num = r1.num*r2.num;
+    r3 -> den = r1.den*r2.den;
+    if (valido_r(r1)==0 || (valido_r(r2)==0) || r3 == NULL)
+        return 0;
+    else return 1;
+}
+
+int divide_r (struct racional r1, struct racional r2, struct racional *r3)
+{
+    r3 -> num = r1.num*r2.den;
+    r3 -> den = r1.den*r2.num;
+    if (valido_r(r1)==0 || (valido_r(r2)==0) || r3 == NULL)
+        return 0;
+    else return 1;
+}
+
+int compara_r (struct racional r1, struct racional r2)
+{
+    if (valido_r(r1) || valido_r(r2))
+        return -2;
+
+    if ((r1.num == r2.num) && (r1.den == r2.den))
+        return 0;
+
+    int min_mult;
+    min_mult = mmc(r1.den, r2.den);
+
+    r1.num = r1.num * (min_mult/r1.den);
+    r2.num = r2.num * (min_mult/r2.den);
+
+    if ((r1.num) < (r2.num))
+        return -1;
+    if ((r1.num) > (r2.num))
+        return 1;
+
+    return 0;
+}
--- tp2/tp2.c
+++ tp2/tp2.c
@@ -6,13 +6,145 @@
 
 /* coloque aqui seus includes (primeiro os <...>, depois os "...") */
 #include <stdio.h>
+#include <stdlib.h>
+#include "racional.h"
 
 /* coloque aqui as funções auxiliares que precisar neste arquivo */
+void quick_sort(struct racional vet[], int n){
+    if (n <= 1)
+        return;
 
+    struct racional temp;
+    int novo_pivo;
+
+    int menor = 0;
+    int maior = n - 1;
+
+    //Escolhe o pivo como o ultimo elemento
+    struct racional pivo = vet[maior];
+    int i = menor - 1;
+
+    //Particionamento
+    for (int j = menor; j < maior; j++){
+        if (compara_r(vet[j], pivo) < 0){
+            i++;
+            //troca vet[i] com vet[j]
+            temp = vet[i];
+            vet[i] = vet[j];
+            vet[j] = temp;
+        }
+    }
+
+    // coloca o pivo na posicao correta
+    temp = vet[i + 1];
+    vet[i + 1] = vet[maior];
+    vet[maior] = temp;
+    novo_pivo = i + 1;
+
+    // recursao
+    quick_sort(vet, novo_pivo); // parte esquerda
+    quick_sort(vet + novo_pivo, n - novo_pivo - 1); // parte da direita
+}
+
+void le_vet(struct racional vetor[], long tam){
+    long i,numerador,denominador;
+    for(i = 0; i < tam; i++)
+    {
+        scanf("%ld %ld", &numerador, &denominador);
+        vetor[i] = cria_r(numerador, denominador);
+    }
+    return;
+}
+
+void elimina_nulo(struct racional vetor[], long tam, long *qtde_nulos)
+{
+    int i,j,tam_temp;
+    tam_temp = tam;
+    for (i = 0; i < tam; i++)
+    {
+        if (valido_r(vetor[i]) == 0)
+        {
+            *qtde_nulos = *qtde_nulos + 1;
+            for (j = tam_temp - 1; j > 1; j--)
+            {
+                if (valido_r(vetor[j]) != 0)
+                {
+                    vetor[i] = vetor[j];
+                    tam_temp = j;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+void soma_vet(struct racional vetor[], long tam)
+{
+    struct racional soma;
+    soma.num = 0;
+    soma.den = 1;
+    struct racional temp;
+    for (int i = 0; i < tam; i++)
+    {
+        soma_r(vetor[i],soma,&temp);
+        soma.num = temp.num;
+        soma.den = temp.den;
+    }
+    imprime_r(soma);
+}
+
+void imprime_vet(struct racional vetor[], long tam)
+{
+    for (int i = 0; i < tam; i++)
+    {
+        printf (" ");
+        imprime_r(vetor[i]);
+    }
+}
 /* programa principal */
 int main ()
 {
-  /* a completar! */
+    long n,nulos;
+    struct racional vetor[100];
 
-  return (0) ;
+    scanf("%ld", &n);
+
+    le_vet(vetor,n);
+
+    printf ("VETOR =");
+    imprime_vet(vetor,n);
+    printf ("\n");
+
+    //eliminar os elementos invalidos do vetor
+    nulos = 0;// quantidade de numeros nulos no vetor
+    elimina_nulo(vetor,n,&nulos);
+
+    printf ("VETOR =");
+    if (nulos != n)
+    {
+        imprime_vet(vetor,n - nulos);
+    }
+    printf ("\n");
+
+    //ordena vetor
+    printf("VETOR =");
+    if (nulos != n)
+    {
+        quick_sort(vetor,(n - nulos));
+        imprime_vet(vetor,n - nulos);
+    }
+    printf ("\n");
+
+    //soma vetor
+    printf("SOMA =");
+    if (nulos != n)
+    {
+        printf(" ");
+        soma_vet(vetor,(n - nulos));
+    }
+    else printf(" 0");
+
+    printf("\n");
+
+    return (0) ;
 }

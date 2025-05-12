359df5b81e005aaf6218cbc033e7245f0ea20c44
Leonardo <leonardomartinslopes2@gmail.com>
2025-05-12 14:43:27 -0300

    [add] Adicao tp4

---
+++ tp4/lista.c
@@ -0,0 +1,17 @@
+// TAD lista de números inteiros
+// Carlos Maziero - DINF/UFPR, Out 2024
+//
+// Implementação do TAD - a completar
+//
+// Implementação com lista encadeada dupla não-circular
+
+struct item_t
+{
+  // define a estrutura interna do item
+} ;
+
+// estrutura de uma lista
+struct lista_t
+{
+  // define a estrutura interna da lista
+} ;

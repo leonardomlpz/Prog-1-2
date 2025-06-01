49282c1b40d430dfe89557770972a5e5c9c464c8
Leonardo <leonardomartinslopes2@gmail.com>
2025-06-01 18:16:10 -0300

    [add] Adicao de arquivos

---
+++ theboys/estruturas.h
@@ -0,0 +1,51 @@
+#include "conjunto.h"
+#include "fila.h"
+#include "fprio.h"
+
+typedef struct heroi
+{
+    int heroi_id;
+    struct cjto_t *habilidades;
+    int paciencia;
+    int velocidade;
+    int exp;
+    int base;
+} heroi_t;
+
+typedef struct base
+{
+    int base_id;
+    int lotacao_max;
+    struct cjto_t *base_presentes;
+    struct fila_t *espera;
+    int coord_x,cood_y;
+} base_t;
+
+typedef struct missao
+{
+    int id;
+    struct cjto_t *habilidades;
+    int cood_x,cood_y;
+} missao_t;
+
+typedef struct mundo
+{
+    int num_herois;
+    heroi_t *herois;
+    int num_bases;
+    base_t *bases;
+    int num_missoes;
+    missao_t *missoes;
+    int num_hab;
+    int num_compostoV;
+    int cood_x,coord_y;
+    int relogio;
+} mundo_t;
+
+typedef struct evento
+{
+    base_t *base;
+    heroi_t *heroi;
+    missao_t *missao;
+    int tempo;
+} evento_t;
---
+++ theboys/eventos.h
@@ -0,0 +1,30 @@
+#ifndef EVENTOS
+#define EVENTOS
+
+#include "estruturas.h"
+
+int aleat (int min, int max);
+
+evento_t *itens(base_t *base, heroi_t *heroi, missao_t *missao);
+
+void chega(int tempo, heroi_t *heroi, base_t *base,struct fprio_t *lef);
+
+void espera(int tempo, heroi_t *heroi, base_t *base,struct fprio_t *lef);
+
+void desiste(int tempo, heroi_t *heroi, base_t *base,mundo_t *mundo,struct fprio_t *lef);
+
+void avisa(int tempo, heroi_t *heroi,base_t *base,struct fprio_t *lef);
+
+void entra(int tempo, heroi_t *heroi, base_t *base,struct fprio_t *lef);
+
+void sai(int tempo, heroi_t *heroi, base_t *base,mundo_t *mundo,struct fprio_t *lef);
+
+void viaja(int tempo, heroi_t *heroi, base_t *base,mundo_t *mundo,struct fprio_t *lef);
+
+void morre(int tempo, heroi_t *heroi, base_t *base,struct missao *missao, struct fprio_t *lef);
+
+void missao(int tempo,mundo_t *mundo, struct missao *missao, struct fprio_t *lef);
+
+void fim(mundo_t *mundo);
+
+#endif
